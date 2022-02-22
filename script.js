a5d6808465a0f20e7dafd3ccaeef505c05af5d9d
Aram Hekimian <aramhek121@gmail.com>
2022-02-22 16:43:31 -0600

    Fixed New grid function

--- script.js
+++ script.js
@@ -1,29 +1,80 @@
-// Make a grid of divs
 let gridContainer = document.querySelector(".gridContainer");
 let newGridSide = 16;
 let newGrid = newGridSide * newGridSide;
+let newSize = (500 / newGridSide) + "px";
+function main() {
+    // Make a grid of divs
 
+    for (let i = 1; i <= newGrid; i++) {
+        let gridElem = document.createElement("div");
+        gridContainer.appendChild(gridElem);
+        gridElem.classList.add("square")
+    }
 
-for (let i = 1; i <= newGrid; i++) {
-    let gridElem = document.createElement("div");
-    gridContainer.appendChild(gridElem);
-    gridElem.classList.add("square")
-}
+    // Make divs change color when you hover over them
+    let square = document.querySelectorAll(".square");
+    for (let i = 0; i < square.length; i++) {
+        square[i].addEventListener("mouseenter", function () {
+            square[i].style.backgroundColor = "black";
+        })
+    }
 
-// Make divs change color when you hover over them
-let square = document.querySelectorAll(".square");
-for (let i = 0; i < square.length; i++) {
-    square[i].addEventListener("mouseenter", function () {
-        square[i].style.backgroundColor = "black";
-    })
-}
-// Add a button that will clear the current grid
-let reset = document.querySelector(".reset");
-for (let i = 0; i < square.length; i++) {
+    // Add a button that will clear the current grid
+    let reset = document.querySelector(".reset");
+    for (let i = 0; i < square.length; i++) {
+        reset.addEventListener("click", function () {
+            square[i].style.backgroundColor = "white";
+        })
+    }
+    //ask the user for the size of the new grid and resize it
     reset.addEventListener("click", function () {
-        square[i].style.backgroundColor = "white";
+        newGridSide = parseInt(prompt("How many squares would you like the new sketch pad to be? limit: 100"));
+        newGrid = newGridSide * newGridSide;
+        while (gridContainer.hasChildNodes()) {
+            gridContainer.removeChild(gridContainer.firstChild);
+        }
+        // make new grid
+        if (newGridSide <= 100) {
+            for (let i = 1; i <= newGrid; i++) {
+                let gridElem = document.createElement("div");
+                gridContainer.appendChild(gridElem);
+                gridElem.classList.add("square")
+                newSize = (500 / newGridSide);
+
+                gridElem.style.width = `${newSize}px`;
+                gridElem.style.height = `${newSize}px`;
+
+
+            }
+        }
+        else {
+            alert("Please input a number from 1 to 100.")
+        }
+
+        // make hover effect for new grid
+        let square = document.querySelectorAll(".square");
+
+        for (let i = 0; i < square.length; i++) {
+            square[i].addEventListener("mouseenter", function () {
+                square[i].style.backgroundColor = "black";
+            })
+        }
+
+
     })
+
+    // set the starting size
+    for (let i = 0; i < square.length; i++) {
+        square[i].style.width = "31.25px";
+        square[i].style.height = "31.25px";
+    }
 }
-// On the same button, ask the user the size of the new grid and make new grids
+
+
+
+
+
+main();
+
 
 // Add a "rainbow" colors button
